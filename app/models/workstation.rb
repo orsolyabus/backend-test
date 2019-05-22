d0438881cdfa44db3add62a9683e2431c5be8cf5
Simon Lau <superslau@gmail.com>
2019-05-22 16:15:25 +0900

    Change `Workstation` to not be based on `ActiveRecord` and fix parameter names

--- app/models/workstation.rb
+++ app/models/workstation.rb
@@ -1,40 +1,40 @@
 # frozen_string_literal: true
 
-class Workstation < ApplicationRecord
+class Workstation 
   DATA = [
     {
       id: 101,
       name: "Dave's workstation",
-      ipAddress: '192.168.1.1',
+      ip_address: '192.168.1.1',
       status: 'running',
-      instanceType: 'g3.4xlarge',
+      instance_type: 'g3.4xlarge',
       platform: 'linux',
       users: [101]
     },
     {
       id: 102,
       name: "Simon's workstation",
-      ipAddress: '192.168.1.2',
+      ip_address: '192.168.1.2',
       status: 'running',
-      instanceType: 'g3.4xlarge',
+      instance_type: 'g3.4xlarge',
       platform: 'linux',
       users: [102]
     },
     {
       id: 103,
       name: 'Windows Workstation',
-      ipAddress: '192.168.1.3',
+      ip_address: '192.168.1.3',
       status: 'stopped',
-      instanceType: 'g3.8xlarge',
+      instance_type: 'g3.8xlarge',
       platform: 'windows',
       users: [101, 102]
     },
     {
       id: 104,
       name: 'Backup Workstation',
-      ipAddress: '192.168.1.4',
+      ip_address: '192.168.1.4',
       status: 'stopped',
-      instanceType: 'g3.4xlarge',
+      instance_type: 'g3.4xlarge',
       users: []
     }
   ].freeze
